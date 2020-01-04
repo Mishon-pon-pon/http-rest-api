5822449668e1f3c73309fef2a87a8e00d51f1a34
Mishon-pon-pon <terem848@gmail.com>
2020-01-04 21:51:41 +0300

    user validate

--- app/model/user.go
+++ app/model/user.go
@@ -2,12 +2,25 @@ package model
 
 import "golang.org/x/crypto/bcrypt"
 
+import validation "github.com/go-ozzo/ozzo-validation"
+
+import "github.com/go-ozzo/ozzo-validation/is"
+
 // User ...
 type User struct {
 	ID                int
 	Email             string
 	Password          string
 	EncryptedPassword string
+}
+
+// Validate ...
+func (u *User) Validate() error {
+	return validation.ValidateStruct(
+		u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.By(requredIf(u.EncryptedPassword == "")), validation.Length(6, 100)),
+	)
 }
 
 // BeforeCreate ...
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a // indirect
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/gorilla/mux v1.7.3
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/lib/pq v1.3.0
